8365a6a713906af42266e790c1f8eacb8b88c62b
xiezhicheng <xiezhicheng1727@126.com>
2018-06-27 18:24:35 +0800

    real time audio record and process

---
+++ real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/alsa_audio_record.cpp" "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/alsa_audio_record.cpp.o"
+  "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/audio_record_process_thread.cpp" "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/audio_record_process_thread.cpp.o"
+  "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/main.cpp" "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/main.cpp.o"
+  "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/mic_array_processing.cpp" "/home/xzc/kdevelop_proj/array_processing/real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/mic_array_processing.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/xzc/eigen3.3.4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ real_time_audio_process/build/CMakeFiles/real_time_audio_process.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/real_time_audio_process.dir/mic_array_processing.cpp.o"
+  "CMakeFiles/real_time_audio_process.dir/audio_record_process_thread.cpp.o"
+  "CMakeFiles/real_time_audio_process.dir/alsa_audio_record.cpp.o"
+  "CMakeFiles/real_time_audio_process.dir/main.cpp.o"
+  "real_time_audio_process.pdb"
+  "real_time_audio_process"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/real_time_audio_process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
